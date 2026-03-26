6303f49c1bfeab96037a4943b406688e4ac8d7c5
신민호 <68156836+alsh0807@users.noreply.github.com>
2026-03-26 13:13:44 +0900

    fix: Update operations.clj

--- src/functions/operations.clj
+++ src/functions/operations.clj
@@ -15,10 +15,10 @@
   ((ops op) a b))
 
 (comment
-  ;; calc에 넣는 op가 ops의 키워드, op를 쓰려면 ':'를 붙여야함 
+  ;; calc에 넣는 op가 ops의 키워드, 키워드를 쓰려면 ':'를 붙여야함 
+  
+  (calc :add 1 2)
+  (calc :sub 10 3)
+  (calc :mul 4 5)
+  (calc :div 20 4)
 )
-
-(calc :add 1 2)
-(calc :sub 10 3)
-(calc :mul 4 5)
-(calc :div 20 4)
